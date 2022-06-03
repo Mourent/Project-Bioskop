b71fd6f11d2c48fe6998de14ace396cf072a5b12
Jefferson Mourent <jmourent@ciputra.student.ac.id>
2022-06-03 11:01:46 +0700

    Connect ke mysql

--- Project Bioskop/Form1.cs
+++ Project Bioskop/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Project_Bioskop
 {
@@ -16,6 +17,13 @@ namespace Project_Bioskop
         {
             InitializeComponent();
         }
+
+        public static string sqlConnection = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DBD_05_BIOSKOP";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlConnection); // sbg data koneksi ke DBMSnya
+        public MySqlCommand sqlCommand; // memindahkan query
+        public MySqlDataAdapter sqlAdapter; // penampung data
+        public MySqlDataReader sqlDataReader;
+        public string sqlQuery;
 
         private void btnGoStudio_Click(object sender, EventArgs e)
         {
